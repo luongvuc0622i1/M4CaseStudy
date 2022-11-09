2b798020481adfe73bf496ddec42fd7bffa140af
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-09 11:15:32 +0700

    vn

---
+++ src/main/java/com/service/trainer/ITrainerService_L.java
@@ -0,0 +1,7 @@
+package com.service.trainer;
+
+import com.model.trainer.Trainer;
+import com.service.IGeneralService;
+
+public interface ITrainerService_L extends IGeneralService<Trainer> {
+}
--- src/main/java/com/service/trainer/TrainerService_L.java
+++ src/main/java/com/service/trainer/TrainerService_L.java
@@ -1,4 +1,27 @@
 package com.service.trainer;
 
-public class TrainerService_L {
+import com.model.trainer.Trainer;
+
+import java.util.Optional;
+
+public class TrainerService_L implements ITrainerService_L {
+    @Override
+    public Iterable<Trainer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Trainer> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Trainer save(Trainer model) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
 }
