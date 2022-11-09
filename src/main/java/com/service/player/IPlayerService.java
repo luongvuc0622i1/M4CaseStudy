79e1ea21de40bc67ca5a32782a98ccbf5892a6d0
Compass <ndphong165@gmail.com>
2022-11-09 10:21:26 +0700

    doing repo player

---
+++ src/main/java/com/service/player/IPlayerService.java
@@ -0,0 +1,12 @@
+package com.service.player;
+
+import com.model.player.Player;
+import com.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IPlayerService extends IGeneralService<Player> {
+//    Page<Player> findAllPage(Pageable pageable);
+//    Page<Player> findPlayerByNameContaining(String name, Pageable pageable);
+
+}
