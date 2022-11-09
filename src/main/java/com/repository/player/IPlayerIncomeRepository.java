8c6688b1ebd00e2ee62b3742c37fde2aa1c8c2ae
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-11-09 11:16:15 +0700

    add annotation repo

--- src/main/java/com/repository/player/IPlayerIncomeRepository.java
+++ src/main/java/com/repository/player/IPlayerIncomeRepository.java
@@ -3,6 +3,8 @@ package com.repository.player;
 import com.model.player.Performance;
 import com.model.player.PlayerIncome;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IPlayerIncomeRepository extends CrudRepository<PlayerIncome,Long> {
 }
