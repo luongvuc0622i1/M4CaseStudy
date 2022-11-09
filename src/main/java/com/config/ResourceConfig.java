2c33f8ebc35e6372901bd70d57a51c9a3e677ab4
Compass <ndphong165@gmail.com>
2022-11-09 16:40:21 +0700

    add ResoureConfig de uploadfile

--- src/main/java/com/config/ResourceConfig.java
+++ src/main/java/com/config/ResourceConfig.java
@@ -1,4 +1,18 @@
 package com.config;
 
-public class ResourceConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+    @Value("${upload_file_avatar}")
+    private String fileUpload;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + fileUpload);
+    }
 }
