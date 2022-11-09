eaa71f64acbe041e1a4a530c070d29c9f2cb09dd
Compass <ndphong165@gmail.com>
2022-11-09 13:57:20 +0700

    push nen tang player service

--- src/main/java/com/service/player/IPlayerService.java
+++ src/main/java/com/service/player/IPlayerService.java
@@ -1,12 +1,21 @@
 package com.service.player;
 
+import com.model.player.Performance;
 import com.model.player.Player;
+import com.model.player.Position;
+import com.model.player.Status;
 import com.service.IGeneralService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IPlayerService extends IGeneralService<Player> {
-//    Page<Player> findAllPage(Pageable pageable);
-//    Page<Player> findPlayerByNameContaining(String name, Pageable pageable);
+    Iterable<Position> findAllPosition();
 
+    Iterable<Performance> findAllPerformance();
+
+    Iterable<Status> findAllStatus();
+
+    Iterable<Player> findAllByName(String name);
+
+    Page<Player> findPage(Pageable pageable);
 }
--- src/main/java/com/service/player/PlayerService.java
+++ src/main/java/com/service/player/PlayerService.java
@@ -1,7 +1,77 @@
 package com.service.player;
 
+import com.model.player.Performance;
 import com.model.player.Player;
+import com.model.player.Position;
+import com.model.player.Status;
+import com.repository.player.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-public class PlayerService{
+import java.util.Optional;
 
+@Service
+public class PlayerService implements IPlayerService{
+
+    @Autowired
+    private IPlayerRepository playerRepository;
+
+    @Autowired
+    private IPositionRepository positionRepository;
+
+    @Autowired
+    private IPerformanceRepository performanceRepository;
+
+    @Autowired
+    private IStatusRepository statusRepository;
+
+    @Autowired
+    private IPlayerIncomeRepository playerIncomeRepository;
+
+    @Override
+    public Iterable<Player> findAll() {
+        return playerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Player> findById(Long id) {
+        return playerRepository.findById(id);
+    }
+
+    @Override
+    public Player save(Player model) {
+        return playerRepository.save(model);
+    }
+
+    @Override
+    public void remove(Long id) {
+        playerRepository.deleteById(id);
+    }
+
+    @Override
+    public Iterable<Position> findAllPosition() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Performance> findAllPerformance() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Status> findAllStatus() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Player> findAllByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Page<Player> findPage(Pageable pageable) {
+        return null;
+    }
 }
