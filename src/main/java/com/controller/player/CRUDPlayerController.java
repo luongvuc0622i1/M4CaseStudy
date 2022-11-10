9cd5a4edbf9ad76ffe1f13a15d3dbecb2d190c2d
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-11-10 15:42:40 +0700

    crud

--- src/main/java/com/controller/player/CRUDPlayerController.java
+++ src/main/java/com/controller/player/CRUDPlayerController.java
@@ -1,20 +1,13 @@
 package com.controller.player;
 
-import com.model.DTO.PlayerDto;
 import com.model.player.Player;
 import com.service.player.IPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,15 +15,9 @@ import java.util.Optional;
 @CrossOrigin("*")
 @RequestMapping("/player")
 public class CRUDPlayerController {
+
     @Autowired
-    private ServletContext servletContext;
-
-
-//    @Autowired
-//    Environment env;
-
-//    @Value("${upload_file_avatar}")
-//    private String upload_file_avatar;
+    Environment env;
 
     @Autowired
     private IPlayerService iPlayerService;
@@ -39,27 +26,6 @@ public class CRUDPlayerController {
         List<Player> players = (List<Player>) iPlayerService.findAll();
         return new ResponseEntity<>(players, HttpStatus.OK);
     }
-//
-//    @PostMapping("/create")
-//    public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
-//        return new ResponseEntity<>(iPlayerService.save(player), HttpStatus.OK);
-//    }
-
-//    @PostMapping("/createAPI")
-//    public ResponseEntity<Player> create(@ModelAttribute PlayerDto player){
-//        Player player1 = new Player(player.getName(), player.getAddress());
-//        MultipartFile multipartFile = player.getImage();
-//        String fileName = multipartFile.getOriginalFilename();
-//        String fileUpload = env.getProperty("upload.path").toString();
-//        try {
-//            FileCopyUtils.copy(player.getImage().getBytes(), new File(fileUpload + fileName));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        player1.setImage(fileName);
-//        iPlayerService.save(player1);
-//        return new ResponseEntity<>(HttpStatus.ACCEPTED);
-//    }
 
     @PutMapping("/player/edit/{id}")
     public ResponseEntity<Player> editPlayer(@PathVariable Long id, @RequestBody Player player) {
@@ -82,5 +48,4 @@ public class CRUDPlayerController {
         iPlayerService.remove(id);
         return new ResponseEntity<>(playerOptional.get(), HttpStatus.NO_CONTENT);
     }
-    
 }
